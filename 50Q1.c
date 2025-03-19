34d6704b53abbad6d3ef4d2ca2c66e1d6401f366
a112332 <a112332@alunos.uminho.pt>
2025-03-19 20:05:34 +0000

    atualização

--- 50Q1.c
+++ 50Q1.c
@@ -341,6 +341,21 @@ int contaPal(char s[]) {
     }
 
     return cont; 
+} 
+
+int contaVogais(char s[]) {
+    char vogais[] = "aeiouAEIOU";
+    int i, j;
+    int numvogais = 0;
+    
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = 0; vogais[j] != '\0'; j++) {
+            if (s[i] == vogais[j]) {
+                numvogais++;
+            }
+        }
+    }
+    return numvogais;
 }
 
 
